23bf516b1476457489994c0276dc56073af219f3
sruthi-itu <srpa@itu.dk>
2021-03-28 12:46:21 +0200

    added comments

--- gaze.py
+++ gaze.py
@@ -12,16 +12,21 @@ class GazeModel:
         self.calibrate()
 
     def calibrate(self):
-
+        #create an empty numpy array
         Dx = np.empty((0,3), float)
         Dy = np.empty((0,3), float)
+        
+        #enumerate
         for i,im in enumerate(self.images):
+            #detect pupil in form ((x, y), (a, b), angle)
             el = detector.find_pupil(im, debug=False)
             #print(el)
-            #((x, y), (a, b), angle)
             center = el[0]
+            #x co-oridnate of center
             cx = center[0]
-            cy = center[1]
+            #y co-oridnate of center
+            cy = center[1]            
+            #stack values to form design matrix of form [x,y,1]
             Dx = np.vstack( (Dx, [cx, cy, 1]) )
             Dy = np.vstack( (Dy, [cx, cy, 1]) )
             #print(self.positions[i], '\t',center)
@@ -29,14 +34,18 @@ class GazeModel:
         #print(Dy)
         self.Dx = Dx
         self.Dy = Dy
+        #fit the values to regression modesl
+        #from pupil centers to the positions from .json file
         self.lrx = LinearRegression().fit(self.Dx, [ x[0] for x in self.positions])
         self.lry = LinearRegression().fit(self.Dy, [ x[1] for x in self.positions])
 
     def estimate(self, image):
         # cv2.imshow('a', cv2.imread(image))
         # cv2.waitKey()
+        #detect pupil in form ((x, y), (a, b), angle)
         el = detector.find_pupil(image, debug=False)
         center = el[0]
         input = [[center[0], center[1], 1]]
-        print(input)
+        #print(input)
+        #return predicted x and y co-ordinates
         return self.lrx.predict(input)[0], self.lry.predict(input)[0]
